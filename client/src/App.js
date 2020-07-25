68652c323d216d27adcd8b4bdeb8e7a0f509e8f6
LJ Spencer <hockeyduck3@gmail.com>
2020-07-25 10:51:06 -0600

    Added basic forgot password page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage from './pages/Homepage/index';
 import SignUp from './pages/Sign Up/index';
 import Signin from './pages/Sign In/index';
+import ForgotPassword from './pages/Forgot-Password/index';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route exact path='/' component={Homepage}/>
                     <Route exact path='/Signup' component={SignUp}/>
                     <Route exact path='/Signin' component={Signin} />
+                    <Route exact path='/Forgot' component={ForgotPassword}/>
                 </Switch>
             </div>
         </Router>
