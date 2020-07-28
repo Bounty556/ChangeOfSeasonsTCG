c20425457a002d12a774133c42b346b0bdba18fb
LJ Spencer <hockeyduck3@gmail.com>
2020-07-27 20:33:04 -0600

    Forgot to hit save

---
+++ client/src/pages/Gameboard/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './gameboard.css';
+
+class Gameboard extends Component {
+    state = {
+        // Add stuff
+    }
+
+    render() {
+        return (
+            <h1>Hello there!</h1>
+        )
+    }
+}
+
+export default Gameboard;
