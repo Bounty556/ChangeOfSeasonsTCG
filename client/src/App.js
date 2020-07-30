1fbe2236c3b75493864e06e78e3df9fceb4bf108
LJ Spencer <hockeyduck3@gmail.com>
2020-07-29 21:11:33 -0600

    Added gameboard

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ import SignUp from './pages/Sign Up/index';
 import Signin from './pages/Sign In/index';
 import ForgotPassword from './pages/Forgot-Password/index';
 import UserProfile from './pages/UserProfile/index';
+import Game from './pages/Gameboard/index';
 
 function App() {
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path='/' component={Homepage}/>
-                    <Route exact path='/Signup' component={SignUp}/>
+                    <Route exact path='/' component={Homepage} />
+                    <Route exact path='/Signup' component={SignUp} />
                     <Route exact path='/Signin' component={Signin} />
-                    <Route exact path='/Forgot' component={ForgotPassword}/>
-                    <Route exact path = '/Profile' component = {UserProfile}/>
+                    <Route exact path='/Forgot' component={ForgotPassword} />
+                    <Route exact path='/Profile' component={UserProfile} />
+                    <Route exact path='/Game' component={Game} />
                 </Switch>
             </div>
         </Router>
--- client/src/pages/Gameboard/index.js
+++ client/src/pages/Gameboard/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Card from '../../components/Card/index';
+
 import './gameboard.css';
 
 class Gameboard extends Component {
@@ -9,7 +11,93 @@ class Gameboard extends Component {
 
     render() {
         return (
-            <h1>Hello there!</h1>
+            <div>
+                <div className='wrapper'>
+                
+                    <div id='opponentRow'>
+                        {/* Opponent's Graveyard*/}
+                        <Card id='opponentGrave'>
+                            <h6>Opponent Graveyard</h6>
+                        </Card>
+
+                        {/* Opponent's Deck */}
+                        <Card id='opponentDeck'>
+                            <h6>Opponent Deck</h6>
+                        </Card>
+
+                        {/* Opponent's Play area */}
+                        <Card id='opponentPlayArea'>
+                            <h4>Opponent Play Area</h4>
+                        </Card>
+                    </div>
+
+                    {/* Defense Row */}
+                    <div id='opponentDefRow'>
+                        <Card id='opponentDef1'>
+                            <h6>Opponent Defense 1</h6>
+                        </Card>
+                        <Card id='opponentDef2'>
+                            <h6>Opponent Defense 2</h6>
+                        </Card>
+                    </div>
+
+                    {/* Attack Row */}
+                    <div id='opponentAttRow'>
+                        <Card id='opponentAtt1'>
+                            <h6>Opponent Attack 1</h6>
+                        </Card>
+                        <Card id='opponentAtt2'>
+                            <h6>Opponent Attack 2</h6>
+                        </Card>
+                        <Card id='opponentAtt3'>
+                            <h6>Opponent Attack 3</h6>
+                        </Card>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className='wrapper'>
+
+                    <div id='userAttRow'>
+                        <Card id='userAtt1'>
+                            <h6>User Attack 1</h6>
+                        </Card>
+                        <Card id='userAtt2'>
+                            <h6>User Attack 2</h6>
+                        </Card>
+                        <Card id='userAtt3'>
+                            <h6>User Attack 3</h6>
+                        </Card>
+                    </div>
+
+                    <div id='userDefRow'>
+                        <Card id='userDef1'>
+                            <h6>User Defense 1</h6>
+                        </Card>
+                        <Card id='userDef2'>
+                            <h6>User Defense 2</h6>
+                        </Card>
+                    </div>
+
+                    <div id='userRow'>
+                        {/* User's Graveyard*/}
+                        <Card id='userGrave'>
+                            <h6>User Graveyard</h6>
+                        </Card>
+
+                        {/* User's Deck */}
+                        <Card id='userDeck'>
+                            <h6>User Deck</h6>
+                        </Card>
+
+                        {/* User's Play area */}
+                        <Card id='userPlayArea'>
+                            <h4>User Play Area</h4>
+                        </Card>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
