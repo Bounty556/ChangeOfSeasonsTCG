91213d6547af50429316893616df82337dd63a80
ZachKellogg <zachskellogg@gmail.com>
2020-07-29 20:47:25 -0600

    adding routes and confirming DB seeds

--- models/user.js
+++ models/user.js
@@ -5,8 +5,33 @@ const UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     avatar: {type: String, default: 'cyclop_01.png'},
+    wins: {type: Number, default: 0},
+    losses: {type: Number, default: 0},
     cardIds: [{type: Number, required: true}]                // User's deck
 });
+
+// Define schema methods
+UserSchema.methods = {
+	checkPassword: function (inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.password)
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10)
+	}
+}
+
+// Define hooks for pre-saving
+UserSchema.pre('save', function (next) {
+	if (!this.password) {
+		console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+		next()
+	} else {
+		console.log('models/user.js hashPassword in pre save');
+		
+		this.password = this.hashPassword(this.password)
+		next()
+	}
+})
 
 const User = mongoose.model('User', UserSchema);
 
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const morgan = require ('morgan');
+const passport = require('passport');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,14 +15,33 @@ if (process.env.NODE_ENV === 'production') {
 // Add routes, both API and view
 app.use(routes);
 
-// Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
 
+// MIDDLEWARE
+app.use(morgan('dev'))
+// app.use(
+// 	bodyParser.urlencoded({
+// 		extended: false
+// 	})
+// )
+// app.use(bodyParser.json())
+
+// Sessions
+// app.use(
+// 	session({
+// 		secret: 'ChangeOfSeasons', //pick a random string to make the hash that is generated secure
+// 		store: new MongoStore({ mongooseConnection: dbConnection }),
+// 		resave: false, //required
+// 		saveUninitialized: false //required
+// 	})
+// )
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/changeOfSeasons');
 
-
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+// Routes
+// app.use('/user', user)
 
 // Start the API server
 app.listen(PORT, function() {
