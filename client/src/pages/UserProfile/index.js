c626662aea95b989ae2f4e0122a705e8b66ae3c4
Andrew Kessler <andrew.kessler95@gmail.com>
2020-08-08 12:57:56 -0600

    got the modal up and working for Avatar selection

--- client/src/pages/UserProfile/index.js
+++ client/src/pages/UserProfile/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, } from 'react';
 //Will be used to go to card lists and deck builder ~possibly friends list if implimented
 // import { Link } from 'react-router-dom';
 
@@ -7,53 +7,75 @@ import Navbar from '../../components/Navbar/index';
 
 import './userProfile.css';
 import phImg from './Images/cyclop_01.png';
-import button from './Images/woodsign.png';
+// import button from './Images/woodsign.png';
 
-class UserProfile extends Component {
-
-    state = {
-        username: 'Username State',
-        //this will be used to let a palyer chose an avatar at one point
-        avatar: '',
-        wins: 0,
-        losses: 0
-    }
+//reactstrap 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <Container>
-                    <div className='card animate__animated animate__slideInDown profileCard '>
-                        <div className='card-body row'>
-                            
-                            <div className='user-avatar col-4'>
-                                <h2>{this.state.username}</h2>
-                                <div>
-                                    <img src={phImg} alt='Player`s Chosen Avatar' className='avatar'></img>
-                                </div>
-                                <p className='chooseAvatar'>Change Avatar (PH)</p>
-                                <div >
-                                    <p className='stats-div'>Wins: {this.state.wins} Losses: {this.state.losses} </p>
-                                </div>
+function UserProfile() {
+    const [username, setUsername] = useState('')
+    const [avatar, setAvatar] = useState('')
+    const [wins, setWins] = useState(0)
+    const [losses, setLosses] = useState(0)
+
+    //code for Modal
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    //Needs 
+    //Write function for setUsername to get the get the users name 
+    //Repeat for wins and losses
+    //create modal for the avatar selection 
+    //create function for setAvatar that actually usues that
+
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <div className='card animate__animated animate__slideInDown profileCard '>
+                    <div className='card-body row'>
+
+                        <div className='user-avatar col-4'>
+                            <h2>{username}</h2>
+                            <div>
+                                <img src={phImg} alt='Player`s Chosen Avatar' className='avatar'></img>
                             </div>
-
-                            <div className='user-links col-4 my-auto'>
-                                <div className='button-col'>
-                                    <button className='wood'>Choose Deck</button>
-                                    <br></br>
-                                    <br></br>
-                                    <button className='wood'>Play Match</button>
-                               </div>
+                            {/* open the modal to select an Avatar */}
+                            <a className='chooseAvatar' onClick={handleShow}>Change Avatar</a>
+                            <div >
+                                <p className='stats-div'>Wins: {wins} Losses: {losses} </p>
                             </div>
-
                         </div>
+
+                        <div className='user-links col-4 my-auto'>
+                            <div className='button-col'>
+                                <button className='wood'>Choose Deck</button>
+                                <br></br>
+                                <br></br>
+                                <button className='wood'>Play Match</button>
+                            </div>
+                        </div>
+
                     </div>
-                </Container>
-            </div>
-        )
-    }
+                </div>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton className='modalBody'>
+                        <Modal.Title className='modalTitle'>Select an Avatar </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className='modalBody'>
+    
+                    </Modal.Body>
+                    <Modal.Footer className='modalBody'>
+                        <Button variant="danger" className='closeButtonModal'  onClick={handleClose}> Close </Button>
+                        <Button variant="primary" className='saveButtonModal' onClick={handleClose}> Save Changes </Button>
+                    </Modal.Footer>
+                </Modal>
+            </Container>
+        </div>
+    )
 
 }
 
