a784e6e348b5a29d34a8decb1af05953b24561ff
Andrew Kessler <andrew.kessler95@gmail.com>
2020-08-09 11:31:52 -0600

    fixed issue I caused with navbar

--- client/src/components/ModalColumn/index.js
+++ client/src/components/ModalColumn/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './modal.css';
+
 
 function ModalColumn(props) {
   return (
   <div>
-    <img src={`./images/cardImg/${props.imageString}.png`}/>
+    <img className='userProfileIcons' src={`./images/cardImg/${props.imageString}.png`}/>
   </div>
   )
 }
