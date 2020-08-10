69bade0d61116b9aec0282c37c1817f4d28a5005
LJ Spencer <hockeyduck3@gmail.com>
2020-08-09 22:43:11 -0600

    Made some styling changes and fixed errors in the console

--- client/src/components/ModalColumn/index.js
+++ client/src/components/ModalColumn/index.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 import './modal.css';
 
 
 function ModalColumn(props) {
   return (
-  <button
-  onClick={props.someFunk}>
-    <img className='userProfileIcons' data={props.imgData} src={`./images/cardImg/${props.imageString}.png`}/>
-  </button>
+    <button onClick={props.someFunk}>
+      <img className='userProfileIcons' data={props.imgData} src={`./images/cardImg/${props.imageString}.png`} alt='Avatar Icon'/>
+    </button>
   )
 }
 
