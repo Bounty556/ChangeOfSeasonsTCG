44bd336c933151c02f86d56620f09083dccc6d1e
LJ Spencer <hockeyduck3@gmail.com>
2020-08-15 09:17:59 -0600

    Made some styling changes to the homepage and the lobby

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../../utils/GlobalState';
 
 import Container from '../../components/Container/index';
 import Navbar from '../../components/Navbar/index';
@@ -8,20 +9,29 @@ import Navbar from '../../components/Navbar/index';
 import './homepage.css';
 
 function Homepage() {
+    const [auth, ] = useAuthContext();
+
     return (
         <div>
             <Navbar />
+            <h1 className=' cosHome animate__animated animate__slideInDown'>Change of Seasons</h1>
+
             <Container>
-                <h1 className=' cosHome animate__animated animate__slideInDown'>Change of Seasons</h1>
-                
                 <div className='card animate__animated animate__slideInDown home-card'>
                     <div className='card-body'>
-                        <img src='./images/buddy.png' alt='placeholder' className='homeImg'></img>
+                        {/* <img src='./images/buddy.png' alt='placeholder' className='homeImg'></img> */}
 
-                        <div className='btnRow'>
-                            <Link  to='/Signup'> <button className='wood'>Sign up</button></Link>
-                            <Link to='/Signin'><button className='wood'>Sign in</button></Link>
-                        </div>
+                        {!auth ? (    
+                            <div className='btnRow'>
+                                <Link  to='/Signup'> <button className='wood'>Sign up</button></Link>
+                                <Link to='/Signin'><button className='wood'>Sign in</button></Link>
+                            </div>
+                        ) : (
+                            <div className='btnRow'>
+                                <Link  to='/Profile'> <button className='wood'>Profile</button></Link>
+                                <Link to='/Lobby'><button className='wood'>Play!</button></Link>
+                            </div>
+                        )}
                     </div>
                </div>
             </Container>
