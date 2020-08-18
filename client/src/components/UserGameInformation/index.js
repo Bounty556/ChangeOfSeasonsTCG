637f6cbea22f59ad7c558fd3eab52f79fb29ea5c
Andrew Kessler <andrew.kessler95@gmail.com>
2020-08-17 21:21:06 -0600

    started working on functionality of attacking a user

--- client/src/components/UserGameInformation/index.js
+++ client/src/components/UserGameInformation/index.js
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useDrop } from 'react-dnd';
+import ItemTypes from '../../utils/ItemTypes';
+import { CardContext } from '../GameBoard';
+
 
 import Card from '../Card';
 import './UserGameInformation.css';
-
 
 const playerInfo = {
   username: localStorage.getItem('username'),
   avatar: localStorage.getItem('avatar'),
 }
 
+
 function UserGameInformation(props) {
+  
+  const { cardDraggedToPosition, } = useContext(CardContext);
+
+  const [, drop] = useDrop({
+    accept: ItemTypes.CARD,
+    drop: item => cardDraggedToPosition(item.uId, props.id)
+  });
+
   return (
-    <div id={props.id}>
+    <div id={props.id} ref={drop}>
       <Card id='gameInfo'>
         <div id='cardRow'>
             <div className='col gameInfoCol'>
@@ -20,7 +32,7 @@ function UserGameInformation(props) {
             className='gameInfoAvatar'
             />
             {/* placeholder */}
-            <p className='lifeTotal'>25</p>
+            <p className='lifeTotal'>{props.lifeState}</p>
             </div>
         </div>
       </Card>
